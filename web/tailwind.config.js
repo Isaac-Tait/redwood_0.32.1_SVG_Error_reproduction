3a41dc43f19b8363668184168d610beb7fce41ff
Isaac Tait <isaac@mountaintopcoding.com>
2021-05-07 09:09:20 -0700

    add tailwind gradients & hero patterns

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,51 @@
+const heropatterns = require('tailwindcss-hero-patterns/src/patterns')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      blue: {
+        DEFAULT: '#4391cdff',
+      },
+    },
+    linearGradientColors: (theme) => theme('colors'),
+    heroPatterns: {
+      topography: heropatterns.topography,
+    },
+    heroPatternsShades: ['100', '300', '500'],
+    heroPatternsColors: ['blue'],
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 5s ease-in forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      colors: {
+        blue: {
+          500: '#4391cdff',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-hero-patterns'),
+    require('tailwindcss-gradients'),
+  ],
 }
